9010e169ecf0d2225da80528ce0a46a947cef26e
thinkdavid <dab4au@virginia.edu>
2019-09-10 13:28:36 -0400

    started skeleton for authentication

---
+++ authenticate.js
@@ -0,0 +1,10 @@
+import requests
+
+payload = {'client_id': '77bf03d75ce441e38287e089b1cb4e4c', 'response_type': 'code', 'redirect_uri': 'http://thinkdavid.xyz/#/store', 'scope': 'user-read-private%20user-read-email&state=34fFs29kd09'}
+resp = requests.get("https://accounts.spotify.com/authorize", params=payload)
+print(resp.url)
+if resp.status_code != 200:
+	print("error")
+else:
+	print("authenticated")
+
